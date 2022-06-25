170c796809da6e50d95a8569d1c8141aca519097
Gaurav Patil <90321047+gaurav1patil@users.noreply.github.com>
2022-06-25 21:32:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app=Flask(__name__,template_folder='./templates')
+
+model = pickle.load(open("model.pkl", "rb"))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    if prediction == 0:
+        output = "This song is not popular."
+    else : 
+        output = "This song is poplar."
+    return render_template('index.html', result=output)
+
+
+if __name__ =="__main__":
+    app.run()
